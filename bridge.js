f1affcc925b5398aded94cbabd744235a3614e71
radicise <93229379+radicise@users.noreply.github.com>
2022-02-06 13:04:17 -0500

    Web support, works but buggy and no verification
    Does not pick up messages sent at very beginning of session
    Verification not yet implemented
    Some message groups are received as singular messages

--- bridge.js
+++ bridge.js
@@ -1,61 +1,102 @@
-'use strict'
-var serverPort = 15227
-var serverIp = "127.0.0.1"
-var ws = require("ws")
-var net = require("net")
-var serv = new ws.Server({"port":15226})
+'use strict';
+console.log("starting...");
+var serverPort = 15227;
+var serverIp = "127.0.0.1";
+var ws = require("ws");
+var net = require("net");
+var serv = new ws.Server({"port":15226});
 serv.on("connection", wsock => {
-    var tsock = new net.Socket().connect(serverPort, serverIp)
+    var tsock = new net.Socket().connect(serverPort, serverIp);
+    tsock.on("data", dat => {
+        console.log(dat.toString("hex"));
+        switch (dat[0]) {
+            case (13):
+                wsock.send("disc" + "serv" + dat.slice(1).toString("utf8"));
+                tsock.end();
+                tsock.destroy();
+                wsock.close();
+                wsock.terminate();
+                break;
+            case (7):
+                wsock.send("mess" + dat.slice(3).toString("utf8"));
+                break;
+            case (1):
+                wsock.send("nonc" + dat.slice(1).toString("hex"));
+                break;
+            case (20):
+                var oper
+                if ((dat[1] & 2) == 2) {
+                    oper = "R";
+                }
+                else {
+                    oper = "C";
+                }
+                if ((dat[1] & 1) == 1) {
+                    oper += "B";
+                }
+                else {
+                    oper += "F";
+                }
+                wsock.send("colo" + oper + dat.slice(2).toString("hex"));
+                break;
+            case (36):
+                wsock.send("verp" + dat.slice(1).toString("hex"))
+        }
+    });
+    tsock.on("close", ifErr => {
+        if (ifErr) {
+            wsock.send("messbridge: server unexpectedly disconnected because of a socket error");
+        }
+        else {
+          wsock.send("messbridge: server unexpectedly disconnected");
+        }
+        wsock.send("discbrigbridge: server endpoint error")
+        wsock.close();
+        wsock.terminate();
+    });
     wsock.on("message", data => {
-        let whole = data.toString("utf8")
-        console.log(whole)
-        var type = whole.substring(0, 4)
-        var mess = whole.substring(4)
+        let whole = data.toString("utf8");
+        console.log(whole);//remove eventually
+        var type = whole.substring(0, 4);
+        var mess = whole.substring(4);
         switch (type) {
             case ("mess"):
-                console.log(mess)//remove later
-                let siz = Buffer.alloc(4)
-                siz.writeInt32BE(Buffer.byteLength(mess, "utf8"))
-                tsock.write(Buffer.concat([Buffer.from([7]), siz, Buffer.from(mess, "utf8")]))
-                break
+                let siz = Buffer.alloc(2);
+                siz.writeInt16BE(Buffer.byteLength(mess, "utf8"));
+                tsock.write(Buffer.concat([Buffer.from([7]), siz, Buffer.from(mess, "utf8")]));
+                break;
             case ("disc"):
-                tsock.write(Buffer.from([13]))
-                tsock.end()
-                tsock.destroy()
-                wsock.destroy()
-                break
+                tsock.write(Buffer.from([13]));
+                tsock.end();
+                tsock.destroy();
+                wsock.terminate();
+                break;
             case ("vers"):
-                tsock.write(Buffer.from("07" + mess, "hex"))
-                break
+                tsock.write(Buffer.from("07" + mess, "hex"));
+                break;
             case ("veri"):
-                let size = Buffer.alloc(4)
-                size.writeInt32BE(Buffer.byteLength(mess.substring(64), "utf8"))
-                tsock.write(Buffer.concat([Buffer.from("0b" + mess.substring(0, 64), "hex"), size, Buffer.from(mess.substring(64), "utf8")]))
-                break
+                let size = Buffer.alloc(2);
+                size.writeInt16BE(Buffer.byteLength(mess.substring(64), "utf8"));
+                tsock.write(Buffer.concat([Buffer.from("0b" + mess.substring(0, 64), "hex"), size, Buffer.from(mess.substring(64), "utf8")]));
+                break;
             default:
-                tsock.write("\u0007client disconnected because of invalid data type", "utf8")
-                tsock.write(Buffer.from([13]))
-                tsock.end()
-                tsock.destroy()
-                wsock.send("discbridge: invalid data type from client: type \"" + type + "\", closing bridge")
-                wsock.end()
-                wsock.destroy()
+                tsock.write(Buffer.from([13]));
+                tsock.end();
+                tsock.destroy();
+                wsock.send("discbridge: invalid data type from client: type \"" + type + "\", closing bridge");
+                wsock.end();
+                wsock.terminate();
         }
-    })
+    });
     wsock.on("error", code => {
-        tsock.write("\u0007client disconnected because of error \"" + JSON.stringify(code) + "\"", "utf8")
-        tsock.write(Buffer.from([13]))
-        tsock.end()
-        tsock.destroy()
-        wsock.destroy()
-    })
+        tsock.write(Buffer.from([13]));
+        tsock.end();
+        tsock.destroy();
+        wsock.terminate();
+    });
     wsock.on("close", hadErr => {
-        if (hadErr) {
-            tsock.write("\u0007client disconnected because of an error", "utf8")
-        }
-        tsock.write(Buffer.from([13]))
-        tsock.end()
-        tsock.destroy()
-        wsock.destroy()
-    })
-})
+        tsock.write(Buffer.from([13]));
+        tsock.end();
+        tsock.destroy();
+    });
+});
--- interfacing.js
+++ interfacing.js
@@ -2,50 +2,80 @@ const queries = new URLSearchParams(window.location.search)
 const tex = document.getElementById("textarea")
 const redge = document.getElementById("textin")
 const conn = new WebSocket(queries.get("bridge"))
-redge.addEventListener("keydown", function(event) {
-    if (event.key == "Enter") {
-        if (redge.value.length > 0) {
-            send(redge.value)
-        }
-        redge.value = ""
-    }
-})
-conn.addEventListener("message", function(event) {
-    var type = event.data.substring(0,4)
-    switch (type) {
-        case ("disc"):
-            conn.send("discdisconnected")//await
-            display("Disconnected!")
-            break
-        case ("mess"):
-            display(event.data.substring(4))
-            break
-        case ("colo"):
-            //switch color
-            break
-        default:
-            display("invalid data from bridge: message type \"" + type + "\"")
-    }
-})
-function send(message) {
-    if (message == "/exit") {
-        //await conn.send("discexit")
-        //await conn.close()
-        window.location.href = "index.html"
-        return
-    }
-    if (message == "/help") {
-        display("\"/exit\" - Exits<br>\"!help\" - Shows server side commands")
-        return
-    }
-    if (message.length > 8000) {
-        display("messages may not be in excess of 8000 characters")
-    }
-    else {
-        display(message)//Remove eventually
-        conn.send("mess" + message)
-    }
-}
+const version = 0.4
+const uname = "guest-testingUser"
+const password = "paswo"
+verHex = "vers3fd999999999999a"
+display("Connecting...")
 function display(text) {
-    tex.innerHTML += (text + "<br>")//Don't do this in final revisions, also prevent from HTML spoofing and "dangerous" characters
+    tex.innerHTML += (text + "<br>")//Don't do this in final revisions, also prevent HTML spoofing and "dangerous" characters
 }
+conn.addEventListener("open", function(event) {
+    display("Connected!")
+    conn.send(verHex)
+    redge.addEventListener("keydown", function(event) {
+        if (event.key == "Enter") {
+            if (redge.value.length > 0) {
+                send(redge.value)
+            }
+            redge.value = ""
+        }
+    })
+    conn.addEventListener("message", function(event) {
+        var type = event.data.substring(0,4)
+        var mess = event.data.substring(4)
+        switch (type) {
+            case ("disc"):
+                conn.send("discdisconnected")
+                display("disconnected by point ID \"" + mess.substring(0, 4) + "\" for: " + mess.substring(4))
+                break
+            case ("mess"):
+                display(event.data.substring(4))
+                break
+            case ("colo"):
+                //switch color
+                break
+            case ("nonc"):
+                /*let logi = crypto.subtle.digest("SHA-256", (new TextEncoder).encode(uname + "/" + password))
+                let login = await logi
+                let numbin = new Uint8Array(32)
+                for (let i = 0; i < 32; i++) {
+                    numbin[i] = parseInt(mess.substring(2 * (i + 2), 2 * (i + 3)), 16)
+                }
+                let nameData = (new TextEncoder).encode(uname)
+                let fin = new Uint8Array(64 + nameData.length)
+                fin.set(new Uint8Array(login))
+                fin.set(numbin, 32)
+                fin.set(nameData, 64)
+                let finHash = crypto.subtle.digest("SHA-256", fin)
+                let finView = new Uint8Array(await(finHash))
+                let out = "veri"
+                finView.forEach(valu => {
+                    out += valu.toString(16).padStart(2, "0")
+                })*/
+                conn.send("veriffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff" + uname)//verification not yet implemented
+                break
+            default:
+                display("client: invalid data type from bridge: type \"" + type + "\"")
+        }
+    })
+    function send(message) {
+        if (message == "/exit") {
+            conn.send("discexit")
+            conn.close()
+            window.location.href = "index.html"
+            return
+        }
+        if (message == "/help") {
+            display("\"/exit\" - Exits<br>\"!help\" - Shows server side commands")
+            return
+        }
+        if (message.length > 8000) {
+            display("messages may not be in excess of 8000 characters")
+        }
+        else {
+            display(message)//Remove eventually
+            conn.send("mess" + message)
+        }
+    }
+})
